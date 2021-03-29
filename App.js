833f933eb0accbcb0276acbe8823c323dffae1d7
kumada0240 <kumada.work0240@gmail.com>
2021-03-29 22:52:59 +0900

    firebase add

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Text, View ,TextInput} from 'react-native';
-
 import LoginScreens from './src/screens/LoginScreen';
 
+import { firebaseConfig } from './env';
 import firebase from 'firebase';
-
-export const  firebaseConfig = {
-  apiKey: "AIzaSyC-td2eWeEENnUvYAdi2YvaA9mPLJxpCzg",
-  authDomain: "twister-4c86d.firebaseapp.com",
-  projectId: "twister-4c86d",
-  storageBucket: "twister-4c86d.appspot.com",
-  messagingSenderId: "631835384050",
-  appId: "1:631835384050:web:bd31a96dc9c3f156de186b"
-};
 
 require('firebase/firestore');
 
